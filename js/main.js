1c2305fa28198b6b211c4cb94b1e784b163aeb87
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2024-05-06 03:27:04 -0300

    last changes

--- js/main.js
+++ js/main.js
@@ -36,6 +36,42 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+const swiper2 = new Swiper(".swiper-books", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 400,
+  spaceBetween: 10,
+  autoplay: true,
+  disableOnInteraction: true,
+  delay: 6000,
+
+  pagination: {
+    el: ".swiper-pagination-books",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next-books",
+    prevEl: ".swiper-button-prev-books",
+  },
+
+  breakpoints: {
+    330: {
+      slidesPerView: 1,
+    },
+    670: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 3,
+    }
+  },
+});
+
+
+
 function toggleNav() {
   let nav = document.getElementById("header");
   if (nav.className === "header") {
@@ -65,3 +101,15 @@ section.classList.remove("glaucoma-active")
 closeBtn.addEventListener("click", () =>
 section.classList.remove("glaucoma-active")
 );
+
+let mysBtn = document 
+    .getElementById('scrollbuttonid'); 
+  
+window.addEventListener('scroll', function () { 
+    if (document.body.scrollTop > 20 
+        || document.documentElement.scrollTop > 20) { 
+        mysBtn.style.display = 'block'; 
+    } else { 
+        mysBtn.style.display = 'none'; 
+    } 
+}); 
